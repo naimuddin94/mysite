145e6db0a8485cd05591a2512def688746a5f410
naimuddin94 <developernaimuddin@gmail.com>
2023-03-28 21:28:27 +0600

    Fix some bug

--- components/WebHistroty.js
+++ components/WebHistroty.js
@@ -1,6 +1,7 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Autoplay, EffectCards } from "swiper";
+
+import { EffectCards } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
@@ -10,8 +11,6 @@ import Image from "next/image";
 import Images from "./Images";
 
 export default function WebHistroty() {
-  SwiperCore.use([Autoplay]);
-
   return (
     <div className="lg:mt-20 md:mx-12 mx-6 mt-10 h-fit">
       <h1 className="lg:mb-10 text-center rounded-lg mb-6 shadow-lg hover:shadow-md md:w-1/3 m-auto font-bold text-2xl p-3 text-orange-600 opacity-90 dark:text-red-500">
@@ -23,10 +22,6 @@ export default function WebHistroty() {
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
-        loop={true}
-        autoplay={{
-          delay: 3000,
-        }}
       >
         {Images.map((slideContent, index) => (
           <SwiperSlide key={index}>
